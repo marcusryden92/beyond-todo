8bfc3c84e7e0a4349955edb4b1166418609ff981
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-25 12:11:43 +0100

    ..

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -21,7 +21,6 @@ export default function Main() {
   async function fetchTasks() {
     const tasks = await getTasks();
     setTasks(tasks);
-    console.log(tasks);
   }
 
   function addLocalTask() {
