1fe8c6dfa77b87fba8508a6e102d52d8c814c56c
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-26 11:50:14 +0100

    moved context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,21 @@ import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import { Context } from "./context/Context.jsx";
+
 import PrivateRoutes from "./context/PrivateRoutes.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Login />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path="/main/:user" element={<Main />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Context>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/main/:user" element={<Main />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Context>
   );
 }
