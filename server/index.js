03c62ecb8dc4a004b7afe0824a7406d6d47cfcad
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-26 19:33:10 +0100

    secure to true

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      secure: false, // Set this to true if you're using HTTPS
+      secure: true, // Set this to true if you're using HTTPS
       sameSite: "lax",
     },
   })
