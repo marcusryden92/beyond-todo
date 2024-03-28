62ae7e330aa3fea1beadd6816d14da0f520f6b44
marcusryden92 <marcus.ryden1992@gmail.com>
2024-03-28 22:08:11 +0100

    Added comments

--- server/api/index.js
+++ server/api/index.js
@@ -28,6 +28,7 @@ async function verificationCallback(username, password, callback) {
   if (!matchedPassword) {
     return callback(null, false, { message: "Wrong password" });
   }
+  console.log("VC", user);
   return callback(null, user);
 }
 
@@ -36,10 +37,13 @@ passport.use(strategy);
 
 // Hexadecimal things
 passport.serializeUser(({ username, user_id }, callback) => {
+  console.log("Serializeuser: ", username, " ", user_id);
   callback(null, JSON.stringify({ username, user_id }));
 });
 
 passport.deserializeUser(async (data, callback) => {
+  console.log("Deserializeuser: ", data);
+
   const user = JSON.parse(data);
   callback(null, user);
 });
