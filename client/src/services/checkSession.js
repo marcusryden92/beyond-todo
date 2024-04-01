d3549aca9632aa5a5750175e0eb79d4f07bb8a61
marcusryden92 <marcus.ryden1992@gmail.com>
2024-04-01 20:16:43 +0200

    ,

--- client/src/services/checkSession.js
+++ client/src/services/checkSession.js
@@ -1,5 +1,4 @@
 export async function checkSession(setStatus) {
-
   const url = `${import.meta.env.VITE_SERVER_URL}/session`;
 
   const res = await fetch(url, {
@@ -10,7 +9,7 @@ export async function checkSession(setStatus) {
       "Content-Type": "application/json",
     },
   });
-  const data = await res.json(); // Parse response body as JSON
+  const data = await res.json(); // Parse response body as JSON/
   console.log(res.status);
   setStatus(res.status);
   return { status: res.status, username: data.username }; // Return both status and username
